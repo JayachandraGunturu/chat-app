32771c5d302701ea6cc98c943439fc720838aa81
jayachandra gunturu <gunturujayachandra@gmail.com>
2020-08-04 20:19:40 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const socketio =require('socket.io');
 const http = require('http');
 
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 const publicDirPath = path.join(__dirname,'../public');
 const server = http.createServer(app);
 const io = socketio(server);
